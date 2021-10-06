f5779cb0c4313225e80c41890fac33faa911ee9c
Matrix53 <1079207272@qq.com>
2021-10-06 17:15:01 +0800

    chore: update gitignore

--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -1,7 +1,13 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 public class Lexer {
-    private Map<String, String> map = Map.ofEntries(
+    private boolean hasNext;
+    private boolean hasBuffer;
+    private char buffer;
+    private final InputStream in;
+    private final Map<String, String> map = Map.ofEntries(
             Map.entry("if", "If"),
             Map.entry("else", "Else"),
             Map.entry("while", "While"),
@@ -21,7 +27,48 @@ public class Lexer {
             Map.entry(">", "Gt"),
             Map.entry("==", "Equal"));
 
-    public static void main(String[] args) {
+    public Lexer(InputStream in) {
+        this.in = in;
+        this.hasNext = true;
+    }
 
+    public boolean hasNext() {
+        return hasNext;
+    }
+
+    private char getChar() throws IOException {
+        char res;
+        if (hasBuffer) {
+            res = buffer;
+            hasBuffer = false;
+        } else {
+            int tmp = in.read();
+            if (tmp == -1) {
+                hasNext = false;
+                res = '\u0000';
+            } else {
+                res = (char) tmp;
+            }
+        }
+        return res;
+    }
+
+    private void ungetChar(char src) {
+        hasBuffer = true;
+        buffer = src;
+    }
+
+    public String nextToken() throws IOException {
+        if (!hasNext) return null;
+        String token = "";
+        char ch = getChar();
+
+        token += ch;
+        return token;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Lexer lexer = new Lexer(System.in);
+        while (lexer.hasNext()) System.out.println(lexer.nextToken());
     }
 }
