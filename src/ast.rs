f574576073b3c2538fec6c2fe68a979fbda0a4d0
Matrix53 <1079207272@qq.com>
2021-10-22 16:14:37 +0800

    chore: update ast

--- src/ast.rs
+++ src/ast.rs
@@ -3,6 +3,107 @@ pub struct CompUnit {
 }
 
 pub enum CompUnitItem {
-    Decl,
+    Decl(Decl),
     FuncDef,
+}
+
+pub enum Decl {
+    ConstDecl,
+    VarDecl
+}
+
+pub struct ConstDecl{
+    pub const_def:Vec<ConstDef>,
+}
+
+pub struct ConstDef{
+
+}
+
+pub enum ConstInitVal {
+    
+}
+
+pub struct VarDecl{
+
+}
+
+pub struct VarDef{
+
+}
+
+pub enum InitVal {
+    
+}
+
+pub struct FuncDef{
+
+}
+
+pub enum FuncType {
+    
+}
+
+pub struct FuncFParams{
+
+}
+
+pub struct FuncFParam{
+
+}
+
+pub struct Block{
+
+}
+
+pub enum BlockItem {
+    
+}
+
+pub enum Stmt {
+    
+}
+
+pub struct LVal{
+
+}
+
+pub enum PrimaryExp {
+    
+}
+
+pub enum UnaryExp {
+    
+}
+
+pub enum UnaryOp {
+    
+}
+
+pub struct FuncRParams{
+
+}
+
+pub enum MulExp {
+    
+}
+
+pub enum AddExp {
+    
+}
+
+pub enum RelExp {
+    
+}
+
+pub enum EqExp {
+    
+}
+
+pub enum LAndExp {
+    
+}
+
+pub enum LOrExp {
+    
 }
