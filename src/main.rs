aa6363bee86843a0820c9ccfd2a92ac9f59dff81
Matrix53 <1079207272@qq.com>
2021-10-23 15:55:10 +0800

    chore: continue to update

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,6 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let source = std::fs::read_to_string(&args[1]).unwrap();
     let tokens = Tokenizer::tokenize(&source);
-    let output = Parser::parse(tokens);
+    let output = Parser::parse(&tokens);
     std::fs::write(&args[2], output).unwrap();
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,17 +1,19 @@
-use std::collections::LinkedList;
+use std::collections::{linked_list::Iter, LinkedList};
 
 use super::token::Token;
 
-pub struct Parser {
-    tokens: LinkedList<Token>,
+pub struct Parser<'a> {
+    iter: Iter<'a, Token>,
 }
 
-impl Parser {
-    pub fn parse(tokens: LinkedList<Token>) -> String {
+impl<'a> Parser<'a> {
+    pub fn parse(tokens: &LinkedList<Token>) -> String {
         if tokens.is_empty() {
             panic!("syntax error!");
         }
-        let mut parser = Parser { tokens };
+        let mut parser = Parser {
+            iter: tokens.iter(),
+        };
         parser.parse_comp_unit()
     }
 
