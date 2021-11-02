6f86b3e70e657d6f00fb6032c8c34a2273a967ef
Matrix53 <1079207272@qq.com>
2021-11-02 17:16:52 +0800

    chore: prepare to have dinner

--- src/assigner.rs
+++ src/assigner.rs
@@ -16,13 +16,53 @@ impl<'a> Assigner<'a> {
             var: 0,
         }
     }
+
     pub fn new_func(&mut self) {
         self.block_pos.clear();
         self.while_block_pos.clear();
         self.pre_var = 0;
         self.var = 0;
     }
-    pub fn new_pre_var() -> String {
-        "".to_string()
+
+    pub fn new_pre_var(&mut self) -> String {
+        self.pre_var = self.pre_var + 1;
+        format!("%{}", self.pre_var)
+    }
+
+    pub fn new_var(&mut self) -> String {
+        self.var = self.var + 1;
+        format!("%x{}", self.var)
+    }
+
+    pub fn get_current_block(&self) -> String {
+        let mut block = String::from("b");
+        for item in self.block_pos {
+            block += format!("_{}", item).as_str();
+        }
+        block
+    }
+
+    pub fn get_next_block() -> String {}
+
+    pub fn get_sub_block() -> String {}
+
+    pub fn get_continue_block() -> String {}
+
+    pub fn get_break_block() -> String {}
+
+    pub fn go_sub_block(&mut self) {
+        self.block_pos.push_back(1);
+    }
+
+    pub fn go_parent_block(&mut self) {
+        self.block_pos.pop_back();
+    }
+
+    pub fn go_next_block(&mut self) {
+        if self.block_pos.is_empty() {
+            self.block_pos.push_back(1);
+        } else {
+            *self.block_pos.back_mut().unwrap() += 1;
+        }
     }
 }
