b97da6e33a5a959845fcc3f37327680e844ce9e2
Matrix53 <1079207272@qq.com>
2021-11-18 15:11:52 +0800

    chore: delete some comments

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,8 @@ mod symbol;
 mod token;
 mod tokenizer;
 
-// use std::{collections::LinkedList, primitive};
-
 use parser::Parser;
 use tokenizer::Tokenizer;
-
-// #[derive(PartialEq, Eq)]
-// enum Test {
-//     A,
-// }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -21,15 +14,4 @@ fn main() {
     let tokens = Tokenizer::tokenize(&source);
     let output = Parser::parse(&tokens);
     std::fs::write(&args[2], output).unwrap();
-
-    // let mut a = vec![1, 2, 3, 4, 5];
-    // println!("{}", a.iter().nth(0).unwrap());
-    // println!("{{\n    {}\n}}", "asdad".to_string());
-    // let a = Test::A;
-    // let b = Test::A;
-    // let c = &a;
-    // let d = &b;
-    // println!("{}", c == d);
-    // let a = Vec::new();
-    // println!("{:?}", vec![1, 2]);
 }
